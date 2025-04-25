9678e4d7b344a20808a704d13d633c8537b5f4c4
lkw199711 <lkw199711@163.com>
2025-04-25 13:29:26 +0800

    fix: 媒体封面生成 再纠正.

--- app/services/create_media_poster_job.ts
+++ app/services/create_media_poster_job.ts
@@ -9,10 +9,18 @@ import prisma from '#start/prisma';
 import { path_poster } from '#utils/index';
 import sharp from 'sharp';
 import * as path from 'path'
-import { media_cover_log } from '#utils/log';
+import fs from 'fs';
+import { error_log, media_cover_log } from '#utils/log';
+import { media } from '@prisma/client';
 
 export default class CreateMediaPosterJob {
   private mediaId: number
+  private mediaInfo: media | null = null
+  private mangaCovers: string[] = [] // 存储封面路径
+  private targetWidth = 60 // 目标宽度
+  private targetHeight = 90 // 目标高度
+  private gap = 2 // 图片间隙
+  private outputPath: string = ''// 合并后的图片路径
 
   constructor({ mediaId }: { mediaId: number }) {
     this.mediaId = mediaId
@@ -22,57 +30,64 @@ export default class CreateMediaPosterJob {
     const mangas = await prisma.manga.findMany({
       where: {
         mediaId: this.mediaId,
-        mangaCover: { not: null },
+        mangaCover: { not: '' },
       },
-      take: 4,
+      take: 10,
       select: { mangaId: true, mangaName: true, mangaCover: true },
       orderBy: { updateTime: 'desc' },
     })
 
     if (!mangas.length) return
 
-    const imagePaths = mangas.filter(manga => manga.mangaCover)
+    this.mangaCovers = mangas
+      .filter(manga => fs.existsSync(manga.mangaCover || ''))
       .map(manga => manga.mangaCover) as string[] // 图片路径
-    const outputPath = path.join(path_poster(), `smanga-media-${this.mediaId}.jpg`) // 合并后的图片路径
+    this.outputPath = path.join(path_poster(), `smanga-media-${this.mediaId}.jpg`) // 合并后的图片路径
     // 生成封面
-    await mergeImages(imagePaths, outputPath, 60, 90)
-    const media = await prisma.media.findUnique({ where: { mediaId: this.mediaId } })
+    await this.mergeImages()
+    this.mediaInfo = await prisma.media.findUnique({ where: { mediaId: this.mediaId } })
     await prisma.media.update({
       where: { mediaId: this.mediaId },
-      data: { mediaCover: outputPath },
+      data: { mediaCover: this.outputPath },
     })
 
-    // 记录日志
-    media_cover_log({ mediaId: this.mediaId, mediaName: media?.mediaName, mediaCover: outputPath })
+    return this.outputPath;
+  }
 
-    return outputPath;
+  async mergeImages() {
+    const images: Buffer[] = []
+    for (let i = 0; i < this.mangaCovers.length; i++) {
+      const imagePath = this.mangaCovers[i];
+      await sharp(imagePath).resize(this.targetWidth, this.targetHeight).toBuffer()
+        .then(buffer => images.push(buffer)) // 将每个图片的缓冲区添加到数组中
+        .catch(err => { error_log('[media poster]', `处理图片 ${imagePath} 时出错:${err}`); })
+
+      if (images.length >= 4) break; // 限制最多处理 4 张图片
+    }
+
+    try {
+      // 创建合并后的图像
+      await sharp({
+        create: {
+          width: (this.targetWidth + this.gap) * images.length - this.gap, // 总宽度，减去最后一个间隙
+          height: this.targetHeight, // 高度
+          channels: 4, // RGBA
+          background: { r: 0, g: 0, b: 0, alpha: 1 } // 黑色背景
+        }
+      })
+        .composite(images.map((buffer, index) => ({
+          input: buffer,
+          top: 0,
+          left: index * (this.targetWidth + this.gap) // 水平排列，考虑间隙
+        })))
+        .toFile(this.outputPath); // 保存合并后的图像
+
+      console.log('合并完成，保存至', this.outputPath);
+      // 记录日志
+      media_cover_log({ mediaId: this.mediaId, mediaName: this.mediaInfo?.mediaName, mediaCover: this.outputPath })
+    } catch (error) {
+      console.error('合并图片时出错:', error);
+    }
   }
 }
 
-async function mergeImages(imagePaths: string[], outputPath: string, targetWidth: number, targetHeight: number, gap: number = 2) {
-  const images = imagePaths.map(imagePath => sharp(imagePath).resize(targetWidth, targetHeight).toBuffer());
-
-  try {
-    const buffers = await Promise.all(images);
-
-    // 创建合并后的图像
-    const composite = await sharp({
-      create: {
-        width: (targetWidth + gap) * images.length - gap, // 总宽度，减去最后一个间隙
-        height: targetHeight, // 高度
-        channels: 4, // RGBA
-        background: { r: 0, g: 0, b: 0, alpha: 1 } // 黑色背景
-      }
-    })
-      .composite(buffers.map((buffer, index) => ({
-        input: buffer,
-        top: 0,
-        left: index * (targetWidth + gap) // 水平排列，考虑间隙
-      })))
-      .toFile(outputPath); // 保存合并后的图像
-
-    console.log('合并完成，保存至', outputPath);
-  } catch (error) {
-    console.error('合并图片时出错:', error);
-  }
-}
